ce922d3c0c87a1b50f1611818b22963564880cf0
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-06 18:01:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,21 +22,21 @@ class TicTacToe
     puts " #{@board[6]} " + "|" + " #{@board[7]} " + "|" + " #{@board[8]} "
   end
 
-def input_to_index(input)
-  return input.to_i - 1
-end
+  def input_to_index(input)
+    return input.to_i - 1
+  end
 
-def move(position, char)
-  @board[position] = char
-end
-#
-# def position_taken?(board, index)
-#   !(board[index].nil? || board[index] == " ")
-# end
-#
-# def valid_move?(board, index)
-#   index.between?(0,8) && !position_taken?(board, index)
-# end
+  def move(position, char)
+    @board[position] = char
+  end
+
+  def position_taken?(index)
+    !(@board[index].nil? || board[index] == " ")
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(@board, index)
+  end
 #
 # def turn(board)
 #   puts "Please enter 1-9:"
